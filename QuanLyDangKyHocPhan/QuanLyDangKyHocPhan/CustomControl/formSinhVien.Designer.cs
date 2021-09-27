22f758d0ed1fc2e9afa20b2aedcc2d4ff7d4aa37
haihak <1911147@dlu.edu.vn>
2021-09-27 20:20:01 +0700

    hải đã ở đây

--- QuanLyDangKyHocPhan/QuanLyDangKyHocPhan/CustomControl/formSinhVien.Designer.cs
+++ QuanLyDangKyHocPhan/QuanLyDangKyHocPhan/CustomControl/formSinhVien.Designer.cs
@@ -51,9 +51,10 @@ namespace QuanLyDangKyHocPhan.CustomControl
             this.label2.Enabled = false;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Teal;
-            this.label2.Location = new System.Drawing.Point(38, 117);
+            this.label2.Location = new System.Drawing.Point(51, 144);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(228, 26);
+            this.label2.Size = new System.Drawing.Size(293, 34);
             this.label2.TabIndex = 41;
             this.label2.Text = "Đăng nhập sinh viên";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -64,9 +65,9 @@ namespace QuanLyDangKyHocPhan.CustomControl
             this.panel1.Controls.Add(this.btnClose);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(304, 33);
+            this.panel1.Size = new System.Drawing.Size(405, 41);
             this.panel1.TabIndex = 42;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
@@ -78,13 +79,13 @@ namespace QuanLyDangKyHocPhan.CustomControl
             this.btnHide.ControlBoxType = Guna.UI.WinForms.FormControlBoxType.MinimizeBox;
             this.btnHide.IconColor = System.Drawing.Color.DimGray;
             this.btnHide.IconSize = 15F;
-            this.btnHide.Location = new System.Drawing.Point(230, 0);
-            this.btnHide.Margin = new System.Windows.Forms.Padding(2);
+            this.btnHide.Location = new System.Drawing.Point(307, 0);
+            this.btnHide.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnHide.Name = "btnHide";
             this.btnHide.OnHoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(58)))), ((int)(((byte)(183)))));
             this.btnHide.OnHoverIconColor = System.Drawing.Color.White;
             this.btnHide.OnPressedColor = System.Drawing.Color.Black;
-            this.btnHide.Size = new System.Drawing.Size(38, 33);
+            this.btnHide.Size = new System.Drawing.Size(51, 41);
             this.btnHide.TabIndex = 43;
             // 
             // btnClose
@@ -94,13 +95,13 @@ namespace QuanLyDangKyHocPhan.CustomControl
             this.btnClose.AnimationSpeed = 0.03F;
             this.btnClose.IconColor = System.Drawing.Color.DimGray;
             this.btnClose.IconSize = 15F;
-            this.btnClose.Location = new System.Drawing.Point(266, 0);
-            this.btnClose.Margin = new System.Windows.Forms.Padding(2);
+            this.btnClose.Location = new System.Drawing.Point(355, 0);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnClose.Name = "btnClose";
             this.btnClose.OnHoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(58)))), ((int)(((byte)(183)))));
             this.btnClose.OnHoverIconColor = System.Drawing.Color.White;
             this.btnClose.OnPressedColor = System.Drawing.Color.Black;
-            this.btnClose.Size = new System.Drawing.Size(38, 33);
+            this.btnClose.Size = new System.Drawing.Size(51, 41);
             this.btnClose.TabIndex = 43;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
@@ -108,20 +109,20 @@ namespace QuanLyDangKyHocPhan.CustomControl
             // 
             this.panel2.Controls.Add(this.gunaPictureBox1);
             this.panel2.Controls.Add(this.label2);
-            this.panel2.Location = new System.Drawing.Point(0, 31);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Location = new System.Drawing.Point(0, 38);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(303, 154);
+            this.panel2.Size = new System.Drawing.Size(404, 190);
             this.panel2.TabIndex = 43;
             this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
             // gunaPictureBox1
             // 
             this.gunaPictureBox1.BaseColor = System.Drawing.Color.White;
-            this.gunaPictureBox1.Location = new System.Drawing.Point(84, 0);
-            this.gunaPictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.gunaPictureBox1.Location = new System.Drawing.Point(112, 0);
+            this.gunaPictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.gunaPictureBox1.Name = "gunaPictureBox1";
-            this.gunaPictureBox1.Size = new System.Drawing.Size(128, 115);
+            this.gunaPictureBox1.Size = new System.Drawing.Size(171, 142);
             this.gunaPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.gunaPictureBox1.TabIndex = 0;
             this.gunaPictureBox1.TabStop = false;
@@ -142,7 +143,8 @@ namespace QuanLyDangKyHocPhan.CustomControl
             this.gunaButton2.ForeColor = System.Drawing.Color.White;
             this.gunaButton2.Image = null;
             this.gunaButton2.ImageSize = new System.Drawing.Size(20, 20);
-            this.gunaButton2.Location = new System.Drawing.Point(84, 366);
+            this.gunaButton2.Location = new System.Drawing.Point(112, 450);
+            this.gunaButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.gunaButton2.Name = "gunaButton2";
             this.gunaButton2.OnHoverBaseColor = System.Drawing.Color.DarkCyan;
             this.gunaButton2.OnHoverBorderColor = System.Drawing.Color.Black;
@@ -150,7 +152,7 @@ namespace QuanLyDangKyHocPhan.CustomControl
             this.gunaButton2.OnHoverImage = null;
             this.gunaButton2.OnPressedColor = System.Drawing.Color.Black;
             this.gunaButton2.Radius = 20;
-            this.gunaButton2.Size = new System.Drawing.Size(119, 51);
+            this.gunaButton2.Size = new System.Drawing.Size(159, 63);
             this.gunaButton2.TabIndex = 45;
             this.gunaButton2.Text = "Đăng Nhập";
             this.gunaButton2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -162,9 +164,10 @@ namespace QuanLyDangKyHocPhan.CustomControl
             this.label1.Enabled = false;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Teal;
-            this.label1.Location = new System.Drawing.Point(38, 400);
+            this.label1.Location = new System.Drawing.Point(51, 492);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.Size = new System.Drawing.Size(0, 21);
             this.label1.TabIndex = 41;
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
@@ -188,8 +191,8 @@ namespace QuanLyDangKyHocPhan.CustomControl
             this.guna2TextBox2.IconRight = ((System.Drawing.Image)(resources.GetObject("guna2TextBox2.IconRight")));
             this.guna2TextBox2.IconRightCursor = System.Windows.Forms.Cursors.Hand;
             this.guna2TextBox2.IconRightSize = new System.Drawing.Size(25, 25);
-            this.guna2TextBox2.Location = new System.Drawing.Point(30, 268);
-            this.guna2TextBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.guna2TextBox2.Location = new System.Drawing.Point(40, 330);
+            this.guna2TextBox2.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.guna2TextBox2.Name = "guna2TextBox2";
             this.guna2TextBox2.PasswordChar = '\0';
             this.guna2TextBox2.PlaceholderForeColor = System.Drawing.Color.Gray;
@@ -197,7 +200,7 @@ namespace QuanLyDangKyHocPhan.CustomControl
             this.guna2TextBox2.SelectedText = "";
             this.guna2TextBox2.SelectionStart = 8;
             this.guna2TextBox2.ShadowDecoration.Parent = this.guna2TextBox2;
-            this.guna2TextBox2.Size = new System.Drawing.Size(238, 38);
+            this.guna2TextBox2.Size = new System.Drawing.Size(317, 47);
             this.guna2TextBox2.Style = Guna.UI2.WinForms.Enums.TextBoxStyle.Material;
             this.guna2TextBox2.TabIndex = 44;
             this.guna2TextBox2.UseSystemPasswordChar = true;
@@ -220,8 +223,8 @@ namespace QuanLyDangKyHocPhan.CustomControl
             this.guna2TextBox1.HoverState.Parent = this.guna2TextBox1;
             this.guna2TextBox1.IconLeft = ((System.Drawing.Image)(resources.GetObject("guna2TextBox1.IconLeft")));
             this.guna2TextBox1.IconLeftSize = new System.Drawing.Size(35, 35);
-            this.guna2TextBox1.Location = new System.Drawing.Point(30, 223);
-            this.guna2TextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.guna2TextBox1.Location = new System.Drawing.Point(40, 274);
+            this.guna2TextBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.guna2TextBox1.Name = "guna2TextBox1";
             this.guna2TextBox1.PasswordChar = '\0';
             this.guna2TextBox1.PlaceholderForeColor = System.Drawing.Color.Gray;
@@ -229,17 +232,17 @@ namespace QuanLyDangKyHocPhan.CustomControl
             this.guna2TextBox1.SelectedText = "";
             this.guna2TextBox1.SelectionStart = 19;
             this.guna2TextBox1.ShadowDecoration.Parent = this.guna2TextBox1;
-            this.guna2TextBox1.Size = new System.Drawing.Size(238, 38);
+            this.guna2TextBox1.Size = new System.Drawing.Size(317, 47);
             this.guna2TextBox1.Style = Guna.UI2.WinForms.Enums.TextBoxStyle.Material;
             this.guna2TextBox1.TabIndex = 44;
             this.guna2TextBox1.TextChanged += new System.EventHandler(this.guna2TextBox1_TextChanged);
             // 
             // formSinhVien
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(304, 495);
+            this.ClientSize = new System.Drawing.Size(405, 609);
             this.Controls.Add(this.gunaButton2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.guna2TextBox2);
@@ -247,8 +250,9 @@ namespace QuanLyDangKyHocPhan.CustomControl
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "formSinhVien";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "formSinhVien";
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
